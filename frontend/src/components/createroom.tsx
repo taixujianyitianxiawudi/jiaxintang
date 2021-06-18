2705d76405230f55edb5a53a21ba0d93481dcc74
taixujianyitianxiawudi <408707751@qq.com>
2021-06-18 20:10:21 +0800

    stable version

--- frontend/src/components/createroom.tsx
+++ frontend/src/components/createroom.tsx
@@ -1,53 +1,59 @@
 import { gql, useMutation } from "@apollo/client";
 import { useRef, useState } from "react";
-import * as CreateRoomTypes from './__generated__/CreateRoom'
+import * as CreateRoomTypes from "./__generated__/CreateRoom";
 
 const CREATE_ROOM = gql`
-mutation CreateRoom($createRoomData: RoomCreateInput!) {
-  createRoom(data: $createRoomData) {
-    id
+  mutation CreateRoom($createRoomData: RoomCreateInput!) {
+    createRoom(data: $createRoomData) {
+      id
+    }
   }
-}
 `;
 
 const CreateRoom: React.FC = () => {
-  const [ createRoom, { loading, error }] = useMutation<
-  CreateRoomTypes.CreateRoom,
-  CreateRoomTypes.CreateRoomVariables
-  >(CREATE_ROOM);
-  const [ name, setName ] = useState('');
-  const [ details, setDetails ] = useState('');
+  const [createRoom] =
+    useMutation<
+      CreateRoomTypes.CreateRoom,
+      CreateRoomTypes.CreateRoomVariables
+    >(CREATE_ROOM);
+  const [name, setName] = useState("");
+  const [details, setDetails] = useState("");
   const nameRef = useRef<HTMLInputElement>(null);
   const detailsRef = useRef<HTMLInputElement>(null);
-    return (
+  return (
     <div>
       <p>Enter Room informations</p>
-      <input 
+      <input
         placeholder="name"
         type="text"
-        onChange={e=>setName(e.target.value)}
+        onChange={(e) => setName(e.target.value)}
         ref={nameRef}
       />
-      <input 
+      <input
         placeholder="details"
         type="text"
-        onChange={e=>setDetails(e.target.value)}
+        onChange={(e) => setDetails(e.target.value)}
         ref={detailsRef}
       />
-      <button onClick={()=>{
-        if (nameRef.current !== null) {
-          createRoom({variables: {createRoomData:{name, details}}})
-          .catch(e=>{return("fuck")})
-          nameRef.current.value = '';
-          if (detailsRef.current !== null) {
-            detailsRef.current.value = '';
+      <button
+        onClick={() => {
+          if (nameRef.current !== null) {
+            createRoom({
+              variables: { createRoomData: { name, details } },
+            }).catch((e) => {
+              return "fuck";
+            });
+            nameRef.current.value = "";
+            if (detailsRef.current !== null) {
+              detailsRef.current.value = "";
+            }
           }
-        }
-      }}>
+        }}
+      >
         CreateRoom
       </button>
     </div>
   );
-}
+};
 
 export default CreateRoom;
--- frontend/src/components/deleteroom.tsx
+++ frontend/src/components/deleteroom.tsx
@@ -1,33 +1,34 @@
 import { gql, useMutation } from "@apollo/client";
 
-import * as DeleteRoomTypes from './__generated__/DeleteRoom'
+import * as DeleteRoomTypes from "./__generated__/DeleteRoom";
 
 const DELETE_ROOM = gql`
-mutation DeleteRoom($deleteRoomId: Int!) {
-  deleteRoom(id: $deleteRoomId) {
-    id
+  mutation DeleteRoom($deleteRoomId: Int!) {
+    deleteRoom(id: $deleteRoomId) {
+      id
+    }
   }
-}
 `;
-
 
 interface RoomProps {
   roomId?: any;
 }
 
 const DeleteRoom: React.FC<RoomProps> = ({ roomId }) => {
-  const [ deleteRoom, { loading, error }] = useMutation<
-  DeleteRoomTypes.DeleteRoom,
-  DeleteRoomTypes.DeleteRoomVariables>(DELETE_ROOM);
+  const [deleteRoom] =
+    useMutation<
+      DeleteRoomTypes.DeleteRoom,
+      DeleteRoomTypes.DeleteRoomVariables
+    >(DELETE_ROOM);
   return (
     <button
-      onClick={()=>{
-        deleteRoom({variables:{deleteRoomId: roomId}})
+      onClick={() => {
+        deleteRoom({ variables: { deleteRoomId: roomId } });
       }}
     >
       Delete room {roomId}
     </button>
-  )
-}
+  );
+};
 
 export default DeleteRoom;
